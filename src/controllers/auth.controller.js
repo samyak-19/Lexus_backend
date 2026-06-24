1eb1af7784c2ab3638ba0aa043822bad59547576
samyak-19 <samlinkin19@gmail.com>
2026-06-24 23:35:09 +0530

    feat: implement user registration with prisma and bcrypt

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -3,16 +3,75 @@ const bcrypt = require("bcryptjs");
 
 const register = async (req, res) => {
     try{
-        res.json({
-            message:"Register controller working",
+
+      const {name , email , password} = req.body
+        // valitation
+        if(!name){
+            return res.status(400).json({
+                success: false,
+                message: "name is require",
+            });
+        }
+
+        if(!email){
+            return res.status(400).json({
+                success:false,
+                message:"email is erquire",
+            });
+        }
+
+        if(!password){
+            return res.status(400).json({
+                success:false,
+                message:"password is require",
+            });
+        }
+
+        if(password.length < 8){
+            return res.status(400).json({
+                success:false,
+                message:"password must be at least 8 charactere",
+            });
+        }
+
+        // user exist
+        const existingUser = await prisma.user.findUnique({
+            where:{
+                email
+            },
         });
+
+        if(existingUser){
+            return res.status(409).json({
+                success:false,
+                message: "Email already exists",
+            });
+        }
+
+        // hashed password
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // create user
+        const user = await prisma.user.create({
+            data:{
+                name,
+                email,
+                password: hashedPassword,
+            },
+        });
+
+        return res.status(201).json({
+            success:true,
+            message: "User regestion Succefully",
+        });
+
     }catch(error){
         console.error(error);
 
         res.status(500).json({
             success: false,
             message : "Internal server error",
-        })
+        });
     }
 };
 
